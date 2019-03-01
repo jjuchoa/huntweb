34e9761a5eaa909d87e0be022e7c23b396cb1801
jjuchoa <jbfdujunior@gmail.com>
2019-03-01 17:05:02 -0300

    Armazenando no state

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class main extends Component {
+    state = {
+        products: [],
+    }
+
     componentDidMount() {
         this.loadProducts();
     }
@@ -9,10 +13,18 @@ export default class main extends Component {
     loadProducts = async () => {
         const response = await api.get('/pontosturisticos/');
 
-        console.log(response.data);
+        //console.log(response.data);
+        this.setState({ products: response.data });
     };
 
     render() {
-        return <h1>Hello</h1>;
+        //return <h1>Contagem de pontos turisticos: {this.state.products.length}</h1>;
+        return (
+            <div className = "product-list">
+                {this.state.products.map(product =>(
+                    <h2 key={product.id}>{product.nome}</h2>
+                ))}
+            </div>
+        )
     }
 }
